693de6447355f55b6f2d4bbf6c2b65d65ee753cc
Travis CI <builds@travis-ci.com>
2018-06-10 22:44:12 +0200

    travis fix

--- travis/tag.sh
+++ travis/tag.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -eu
+set -x
 
 SERVICE_NAME=$1
 
 # Create Git tag from the version number in package.json and push to the origin
-git config --global user.email "builds@travis-ci.com"
+git config --global user.email {EMAIL}
 git config --global user.name "Travis CI"
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 GIT_TAG=v$PACKAGE_VERSION
