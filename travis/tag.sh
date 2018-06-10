6507dce3b9f557cc376469e167f23b26e5361c8a
Travis CI <builds@travis-ci.com>
2018-06-10 22:59:24 +0200

    fix travis

--- travis/tag.sh
+++ travis/tag.sh
@@ -4,11 +4,11 @@ set -x
 
 SERVICE_NAME=$1
 
-# Create Git tag from the version number in package.json and push to the origin
-git config --global user.email {EMAIL}
-git config --global user.name "Travis CI"
+# After a successful build to master, we create a tag
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 GIT_TAG=v$PACKAGE_VERSION
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name $GH_TOKEN
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-git remote add deploy https://${GH_TOKEN}@github.com/martshaw/los.git >/dev/null 2>&1
 git push origin $GIT_TAG
+# git push --quiet origin $GIT_TAG > /dev/null 2>&1
