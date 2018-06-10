7e8bbaeb18d54a76d4637e21cf43fe590842cfaf
Travis CI <builds@travis-ci.com>
2018-06-10 20:20:42 +0200

    tagging

--- travis/tag.sh
+++ travis/tag.sh
@@ -9,4 +9,5 @@ git config --global user.name "Travis CI"
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 GIT_TAG=v$PACKAGE_VERSION
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+git remote add deploy https://$GH_TOKEN@github.com/martshaw/los.git >/dev/null 2>&1
 git push origin $GIT_TAG
