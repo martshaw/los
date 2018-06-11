76bbd2516b0d3d3f8aa0f8c7e3dc194b616ddaaa
Martin Shaw <martshaw@users.noreply.github.com>
2018-06-11 18:41:09 +0200

    travis fix

--- travis/tag.sh
+++ travis/tag.sh
@@ -8,7 +8,8 @@ SERVICE_NAME=$1
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 GIT_TAG=v$PACKAGE_VERSION
 git config --global user.email "builds@travis-ci.com"
-git config --global user.name $GH_TOKEN
+git config --global user.email $USERNAME
+git config --global user.password $GH_TOKEN
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 git push origin $GIT_TAG
 # git push --quiet origin $GIT_TAG > /dev/null 2>&1
